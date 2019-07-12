842933f2754995c26e8ca0586d112430c59fef51
Jax Taylor <jax.maximillion.taylor@gmail.com>
2019-07-11 21:22:38 -0400

    Updates

---
+++ precache-manifest.6b5604e0708b6a127c8de95bece1fa0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a1705c8197f6307ea23b50aa744bc7",
+    "url": "/course-directory-2/index.html"
+  },
+  {
+    "revision": "d79fafe521107e2efb67",
+    "url": "/course-directory-2/static/css/main.ceca84a1.chunk.css"
+  },
+  {
+    "revision": "4848d49362a7df4cf2fb",
+    "url": "/course-directory-2/static/js/2.6f180ba9.chunk.js"
+  },
+  {
+    "revision": "d79fafe521107e2efb67",
+    "url": "/course-directory-2/static/js/main.d0a0ffe2.chunk.js"
+  },
+  {
+    "revision": "13996e48be987f6e1795",
+    "url": "/course-directory-2/static/js/runtime~main.fd103425.js"
+  }
+]);
